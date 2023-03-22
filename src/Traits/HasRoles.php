fee8a81048c1247600e2b76cd9b5ed65958ce6bc
Felice <felice.ostuni@uania.com>
2023-03-22 12:52:46 +0100

    wip

--- src/Traits/HasRoles.php
+++ src/Traits/HasRoles.php
@@ -9,4 +9,17 @@ use Spatie\Permission\Traits\HasRoles as UserHasRoles;
 trait HasRoles {
 
     use UserHasRoles;
+
+    public function hasRoleOrPermission($roleOrPermission)
+    {
+        $rolesOrPermissions = is_array($roleOrPermission)
+            ? $roleOrPermission
+            : explode('|', $roleOrPermission);
+
+        if ($this->hasAnyRole($rolesOrPermissions) || $this->hasAnyPermission($rolesOrPermissions)) {
+            return true;
+        }
+
+        return false;
+    }
 }
