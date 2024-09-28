6a70ae92c1a1fdf6941aea84cdf3e2d4e5aa22e6
Felice Ostuni <felice.ostuni@uania.com>
2024-09-28 18:59:06 +0200

    wip socialite fixes

--- Services/SocialiteService.php
+++ Services/SocialiteService.php
@@ -6,36 +6,54 @@ namespace App\Modules\Auth\Services;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 
 class SocialiteService {
     public static function loginOrRegister($driver, $payload){
-        $existingUser = User::where('email', $payload->email)->first();
+        $user = User::where('email', $payload->email)->first();
 
         if(!$payload->email){
             return redirect('/login');
         }
 
-//        $existingTrashedUser = User::where('email', $payload->email)->whereNotNull('deleted_at')->withTrashed()->first();
-//        if($existingTrashedUser){
-//            $existingTrashedUser->forceDelete();
-//        }
+        if(!$user){
 
-        if($existingUser){
-            auth()->login($existingUser, true);
-        } else {
-            $newUser = new User();
-            $newUser->name = $payload->name;
-            $newUser->email = $payload->email;
-            $newUser->password = Hash::make(Str::password(16));
-            $newUser->email_verified_at = Carbon::now()->format('Y-m-d H:i:s');
-            $newUser->google_id = $payload->id;
-            $newUser->avatar = $payload->avatar;
-            $newUser->save();
+            $user = new User();
+            $user->name = $payload->name;
+            $user->email = $payload->email;
+            $user->password = Hash::make(Str::password(16));
+            $user->email_verified_at = Carbon::now()->format('Y-m-d H:i:s');
+            $user->google_id = $payload->id;
+            $user->save();
 
-            auth()->login($newUser, true);
         }
+
+        if($user) {
+            self::updateAvatarFromSocial($user, $payload);
+        }
+
+        auth()->login($user, true);
+
         return redirect()->to('/');
     }
+
+    public static function updateAvatarFromSocial($user, $payload)
+    {
+        $filename = 'avatar.jpg';
+        $url = $payload->getAvatar();
+        if ($url) {
+            $url = str_replace('type=normal','type=large', $url);
+            $contents = file_get_contents($url);
+            Storage::put('public/users/'.$user->id.'/photos/'.$filename, $contents);
+
+            $user->avatar = $filename;
+            $user->save();
+
+        }
+
+
+    }
+
 }
