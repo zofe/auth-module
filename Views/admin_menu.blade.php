5e19836de96a05f4bab0c08661ea0721a0751282
Felice Ostuni <felice.ostuni@gmail.com>
2024-10-15 17:54:49 +0200

    frontend/admin layout navigation

--- Views/admin_menu.blade.php
+++ Views/admin_menu.blade.php
@@ -1,5 +1,5 @@
 
-@if(Auth::user()->hasRoleOrPermission('admin|view everything|edit everything|view users|edit users'))
+@if(Auth::user() && Auth::user()->hasRoleOrPermission('admin|view everything|edit everything|view users|edit users'))
 <x-rpd::nav-dropdown icon="user" label="Auth" active="/auth">
     <x-rpd::nav-link label="Users list" route="auth.users" type="collapse-item" />
     <x-rpd::nav-link label="Role&Permissions" route="auth.permissions" type="collapse-item" />
